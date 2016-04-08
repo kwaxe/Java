403baeb46404d0da300071723f59cf7c35dd041c
Kweku Kwabi <kwaxe@yahoo.com>
2016-04-07 21:26:01 -0500

    updates
    folder structure and some code

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d9/e0a2f5312ffd00151eedb00aa654d99c
@@ -0,0 +1,8 @@
+
+public class ItemDiscount {
+
+	public ItemDiscount() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Assigment 9/src/Cart.java
+++ Assigment 9/src/Cart.java
@@ -4,23 +4,60 @@ public class Cart {
 	private int numItems; // Current number of items in cart
 	private SalesItem[] itemList;
 
-	public Cart(int maxNumOfItems) {
+	public Cart(int maxNumOfItems) {	
 		this.numItems = 0;
 		this.itemList = new SalesItem[maxNumOfItems];
 		
 	}
 	
-	public void addItem(String title, double price, int quantity, int type ){
+	public void addItem(String title, double price, int quantity, SalesItem type ){
 		
-		if(type == 1){
-			Book b = new Book();
+		if(type instanceof Book){
+			type.setCode(1);
 		}
-		else if(type == 2){
-			Dictionary d = new Dictionary();
+		else if(type instanceof Dictionary){
+			type.setCode(2);
 		}
-		else if(type == 3){
-			AudioCD acd = new AudioCD();
+		else if(type instanceof AudioCD){
+			type.setCode(3);
 		}
+	}
+	
+	public double cartTotalCost(){
+		double total = 0;
+		for(int i = 0; i < itemList.length; i++){
+			total = total + itemList[i].itemTotalCost();
+		}
+		return total;
+	}
+	
+	public int getNumBooks(){
+		int books = 0;
+		for(int i = 0; i < itemList.length; i++){
+			if(itemList[i].getCode() == 1){
+				books++;
+			}
+		}
+		return books;
+	}
+	
+	public int getNumCDS(){
+		int cds = 0;
+		for(int i = 0; i < itemList.length; i++){
+			if(itemList[i].getCode() == 3){
+				cds++;
+			}
+		}
+		return cds;
+	}
+	
+	public String toString(){
+		
+		String str = "";
+		for(int i = 0; i < this.numItems; i++ ){
+			str+= "\n" + itemList[i];
+		}
+		return str;
 	}
 
 }
